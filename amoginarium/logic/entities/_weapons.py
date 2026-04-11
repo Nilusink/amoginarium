5a911498f3a8f6ede7c662356ec41b10e0bd6a0a
Nilusink <nilusink@protonmail.com>
2026-04-11 22:47:18 +0200

    fixed weapon positions
    Took 3 minutes

--- amoginarium/logic/entities/_weapons.py
+++ amoginarium/logic/entities/_weapons.py
@@ -216,7 +216,10 @@ class BaseWeapon(Item):
         # if self._current_sound_time < 0:
         #     self._current_sound_time = 0
 
-        super()._update(delta)
+        if self.parent:
+            self.position = self.parent.position + self._parent_position_offset
+
+        super()._update(delta, keep_position=True)
         self._runtime_buffer[self.id].param1, _ = self.get_mag_state(1)
 
     def stop_shooting(self):
