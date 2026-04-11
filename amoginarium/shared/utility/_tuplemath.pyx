63d9379c9d7d2624cf6ac2a3ec11f08a0dcc38f6
LukasKrah <l.krahbichler05@gmail.com>
2026-04-11 22:28:32 +0200

    merge logic & radar

---
+++ amoginarium/shared/utility/_tuplemath.pyx
@@ -0,0 +1,46 @@
+"""
+amoginarium/logic/_tuplemath.pyx
+
+Project: amoginarium
+Created: 03.04.2026
+Authors: LukasKrah
+"""
+
+cimport cython
+
+
+cdef class _TupleMath:
+    """
+    A fast Cython implementation providing element-wise mathematical operations.
+    """
+
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    def add(self, tuple t1, tuple t2) -> tuple:
+        cdef Py_ssize_t i
+        cdef Py_ssize_t n = len(t1)
+        return tuple([t1[i] + t2[i] for i in range(n)])
+
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    def sub(self, tuple t1, tuple t2) -> tuple:
+        cdef Py_ssize_t i
+        cdef Py_ssize_t n = len(t1)
+        return tuple([t1[i] - t2[i] for i in range(n)])
+
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    def mul(self, tuple t1, tuple t2) -> tuple:
+        cdef Py_ssize_t i
+        cdef Py_ssize_t n = len(t1)
+        return tuple([t1[i] * t2[i] for i in range(n)])
+
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    def div(self, tuple t1, tuple t2) -> tuple:
+        cdef Py_ssize_t i
+        cdef Py_ssize_t n = len(t1)
+        return tuple([t1[i] / t2[i] for i in range(n)])
+
+
+TupleMath = _TupleMath()
