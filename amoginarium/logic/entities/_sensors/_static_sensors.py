ebea3a4717cc02a510e8977719705544d52897d1
LukasKrah <l.krahbichler05@gmail.com>
2026-04-18 18:17:00 +0200

    fix

--- amoginarium/logic/entities/_sensors/_static_sensors.py
+++ amoginarium/logic/entities/_sensors/_static_sensors.py
@@ -66,7 +66,7 @@ class VisualSensor(LogicGameEntity):
         pv.COQ.put(
             ProcessCommand(
                 type=BaseCommandType.spawn_dummy,
-                kwargs={"id": self.id, "cid": DummyCIDs.base_bullet, "spawn_time": 0, "position": position},
+                kwargs={"id": self.id, "cid": DummyCIDs.base_bullet, "spawn_time": 0, "position": position.xy},
             )
         )
 
